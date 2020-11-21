524892a19f6b05ad9f3114e7c534c7835d3c6b59
github username <github email address>
2020-11-20 18:03:01 -0600

    commit tree 5db9a8eb6910fa63aebfee73da7785c233ce1904

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,45 @@
+require 'pry'
 
+class CashRegister
+
+    attr_accessor :total, :discount, :items, :new_transaction 
+
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+        @items = []
+        @new_transaction = 0.0
+    end
+
+    def add_item(title, price, quantity = 1)
+        @new_transaction = (price * quantity)
+        @total += @new_transaction
+        quantity.times do
+            @items << title 
+        end
+    end
+
+    def apply_discount
+        #binding.pry
+        if self.discount > 0
+            new_total = @total -= (@total * (@discount.to_f / 100.to_f))
+            "After the discount, the total comes to $#{new_total.to_i}."
+        else
+            "There is no discount to apply."
+        end
+    end
+
+    def items
+        @items
+    end
+
+    def void_last_transaction
+        @items.pop()
+        @total -= @new_transaction
+    end
+
+end
+
+#new_customer = CashRegister.new(0)
+#binding.pry
+0
